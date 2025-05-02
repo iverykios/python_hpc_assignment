ad1df7e14977c0973db0849ac9da4790a0cdce55
Ilias Verykios <iliasverykios@Iliass-Laptop.local>
2025-05-02 23:23:56 +0200

    scirpt to profile with nsys

---
+++ scripts/job_task10.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#BSUB -q c02613
+#BSUB -J profiling_gpu
+
+#BSUB -n 4
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=2GB]"
+
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -W 00:30
+
+#BSUB -o profiling_gpu_%J.out
+#BSUB -e profiling_gpu_%J.err
+
+#BSUB -B
+#BSUB -N
+
+export TMPDIR=$__LSF_JOB_TMPDIR__
+
+nvidia-smi
+
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+
+nsys profile -o jacobi_prof python ../task_9.py 10 
