23a5cd88fb6d90ba1df54117a86d962cf5c986e7
Ilias Verykios <iliasverykios@Iliass-Laptop.local>
2025-05-03 17:28:13 +0200

    change script for differnet queue

--- scripts/job_task10.sh
+++ scripts/job_task10.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#BSUB -q c02613
+#BSUB -q gpua100
 #BSUB -J profiling_gpu
 
 #BSUB -n 4
@@ -8,6 +8,7 @@
 #BSUB -R "rusage[mem=2GB]"
 
 #BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -R "select[gpu40gb]"
 #BSUB -W 00:30
 
 #BSUB -o profiling_gpu_%J.out
@@ -17,10 +18,11 @@
 #BSUB -N
 
 export TMPDIR=$__LSF_JOB_TMPDIR__
-
+lscpu 
 nvidia-smi
 
 source /dtu/projects/02613_2025/conda/conda_init.sh
 conda activate 02613
 
-nsys profile -o jacobi_prof python ../task_9.py 10 
+nsys profile -o jacobi_prof_1 python ../task_9.py 1
+nsys stats jacobi_prof_1 >> data.txt
